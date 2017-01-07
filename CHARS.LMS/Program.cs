6d7346d78729092f1ba2e369c10973fe6ad98b7c
charspacegroup <charspacegroup@gmail.com>
2017-01-07 22:07:06 +0630

    Thura Testing

--- CHARS.LMS/Program.cs
+++ CHARS.LMS/Program.cs
@@ -15,6 +15,7 @@ namespace CHARS.LMS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CharSpace());
+            //
         }
     }
 }
