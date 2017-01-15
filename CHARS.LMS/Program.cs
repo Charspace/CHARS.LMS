971d36ff5c7df156e6ab1312e8c0a3f4b02973ed
Bo-Bo-Aung <boboaung92@gmail.com>
2017-01-15 13:31:31 +0630

    BBA

--- CHARS.LMS/Program.cs
+++ CHARS.LMS/Program.cs
@@ -14,7 +14,7 @@ namespace CHARS.LMS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);            
-            Application.Run(new CharSpace()); ////
+            Application.Run(new CharSpace()); ////bbA
             //thur commit
 
         }
